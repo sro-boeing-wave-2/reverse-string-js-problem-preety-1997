e2cd7a00d42797e72c6492098a8e5cc60faf8aa9
Preety Kumari <preetydakshana@gmail.com>
2018-07-10 12:29:36 +0530

    modified to decrease time complexity

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,7 +1,6 @@
 // The intent of this file is to reverse
 // the string using built in functions.
 const reverse = function reverseString(str) {
-    return (str.split('').reverse().join(''));
-}
-
+  return (str.split('').reverse().join(''));
+};
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,11 +1,15 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-function reverse(str) {
-    var n = str.length;
-    var reversestring = "";
-    for (var z = n; z >= 0; z--) {
-        reversestring += str.charAt(z);
-    }
-    return reversestring;
+function reverse(inputstring) {
+  const str = [];
+  let i;
+  for (i = 0; i < Math.floor((inputstring.length) / 2); i += 1) {
+    str[inputstring.length - i - 1] = inputstring[i];
+    str[i] = inputstring[inputstring.length - i - 1];
+  }
+  if (inputstring.length % 2 === 1) {
+    str[i] = inputstring[i];
+  }
+  return str.join('');
 }
 
 module.exports = reverse;
